f3fc4e7f3ac623ce6e13e75312dd50b5d6274468
Mnkras <mnkras@gmail.com>
2012-09-18 23:34:19 -0400

    initial Commit

---
+++ api/routes/users.php
@@ -0,0 +1,69 @@
+<?php defined('C5_EXECUTE') or die('Access Denied');
+
+class UsersApiRouteController extends ApiRouteController {
+
+	public function run($ID = false) {
+		switch (API_REQUEST_METHOD) {
+			case 'GET':
+				if($ID && $ID > 0) {
+					return $this->getUser($ID);
+				} else {
+					return $this->getList();
+				}
+			
+			default: //BAD REQUEST
+				$this->setCode(400);
+				$this->respond(array('error' => 'Bad Request'));
+		}
+	}
+
+	private function getUser($ID) {
+		$user = UserInfo::getByID($ID); //get the super complicated user object
+		if(!is_object($user) || $user->isError()) { //lets check if this user exists...
+			$this->setCode(404); //NOPE time to 404!
+			$this->respond(array('error' => 'User Not Found'));
+		}
+		return $this->cleanUser($user);
+
+	}
+
+	private function getList() {
+
+		$maxlimit = C5_API_USERS_LIMIT;
+		$limit = C5_API_USERS_LIMIT_DEFAULT;
+		$offset = 0;
+
+		if(isset($_GET['limit']) && $_GET['limit'] > 0 && $_GET['limit'] <= $maxlimit) {
+			$limit = $_GET['limit'];
+		}
+		if(isset($_GET['offset']) && $_GET['offset'] > 0) {
+			$offset = $_GET['offset'];
+		}	
+
+		$pl = new UserList();
+		
+		$list = $pl->get($limit, $offset);
+		//$list = $pl->get(2, $offset);
+		$nlist = array();
+		foreach($list as $user) {
+			$nlist[] = $this->cleanUser($user);
+		}
+		//print_r($nlist);
+		return $nlist;
+
+	}
+
+	private function cleanUser($user) {
+		//$attributes = CollectionAttributeKey::getAttributes($user->cID, $user->vObj->cvID);
+		$attributes = UserAttributeKey::getList();
+		$natt = array();
+		foreach($attributes as $att) {
+			$val = $user->getAttribute($att->getAttributeKeyHandle());
+			$natt[$att->getAttributeKeyHandle()] = (string) $val;
+		}
+		$attr = array('attributes' => $natt);
+		$pobj = $this->filterObject($user, array('uID', 'uDateAdded', 'uIsActive', 'uName', 'uEmail', 'uTimezone'));
+		return $this->object_merge($pobj, $attr);
+	}
+
+}
---
+++ controller.php
@@ -0,0 +1,39 @@
+<?php defined('C5_EXECUTE') or die("Access Denied.");
+
+class ApiBaseUsersPackage extends Package {
+
+	protected $pkgHandle = 'api_base_users';
+	protected $appVersionRequired = '5.6.0';
+	protected $pkgVersion = '0.9';
+
+	public function getPackageName() {
+		return t("Api:Base:Users");
+	}
+
+	public function getPackageDescription() {
+		return t("Get information about specific users");
+	}
+
+	public function on_start() {
+		Config::getAndDefine('C5_API_USERS_LIMIT', 50);
+		Config::getAndDefine('C5_API_USERS_LIMIT_DEFAULT', 10);
+	}
+
+	public function install() {
+		$installed = Package::getByHandle('api');
+		if(!is_object($installed)) {
+			throw new Exception(t('Please install the "API" package before installing %s', $this->getPackageName()));
+		}
+
+		parent::install();
+
+		$pkg = Package::getByHandle($this->pkgHandle);
+		ApiRoute::add('users', t('List users and get information about different users'), $pkg);
+	}
+	
+	public function uninstall() {
+		ApiRouteList::removeByPackage($this->pkgHandle);//remove all the apis
+		parent::uninstall();
+	}
+
+}
